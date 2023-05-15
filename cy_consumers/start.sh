6f0abdb3007ab30259653b0333bca9e484075aa0
nttlong <zugeliang2000@gmail.com>
2023-05-15 15:06:35 +0700

    first commit

--- cy_consumers/start.sh
+++ cy_consumers/start.sh
@@ -6,16 +6,17 @@ source $PYTHON_ENV
 cd $APP_DIR/cy_consumers
 python files_upload.py & \
 python files_generate_thumbs.py & \
-#python files_extrac_text_from_image.py & \
-#python files_generate_image_from_office.py & \
-#python files_generate_image_from_pdf.py & \
-#python files_generate_image_from_video.py & \
-#python files_generate_pdf_from_image.py & \
+python files_extrac_text_from_image.py & \
+python files_generate_image_from_office.py & \
+python files_generate_image_from_pdf.py & \
+python files_generate_image_from_video.py & \
+python files_generate_pdf_from_image.py & \
 
-#python files_ocr_pdf.py & \
-#python files_save_custom_thumb.py & \
-python files_save_default_thumb.py
-#python files_save_orc_pdf_file.py
+python files_ocr_pdf.py & \
+python files_save_custom_thumb.py & \
+python files_save_default_thumb.py  & \
+python files_save_orc_pdf_file.py   & \
+python files_save_search_engine.py
 
 
 
