32ea9654c42550efaed0db430191cb966c7b9c3d
nttlong <zugeliang2000@gmail.com>
2023-05-16 17:34:57 +0700

    first commit

--- cy_consumers/test_get_doc_info.py
+++ cy_consumers/test_get_doc_info.py
@@ -1,8 +1,13 @@
 import cy_kit
 from cyx.common.file_information import FileInformationService
 fx = cy_kit.singleton(FileInformationService)
-fx1=f"/home/vmadmin/python/v6/file-service-02/temp-data/LV - ThuThongBao _002_.docx"
-fx2=f"/home/vmadmin/python/v6/file-service-02/temp-data/lancape.docx"
 
-a = fx.get_total_pages(fx2)
-print(a)
+fx1=f"/home/vmadmin/python/v6/file-service-02/temp-data/lancape.docx"
+fx1 =f"/home/vmadmin/python/v6/file-service-02/temp-data/aaaa.xlsx"
+fx1=f"/home/vmadmin/python/v6/file-service-02/temp-data/xxxxx.xlsx"
+# fx2=f"/home/vmadmin/python/v6/file-service-02/temp-data/lancape.docx"
+#
+a = fx.get_total_pages(fx1)
+# print(a)
+
+fx.get_one_page(fx1,0)
--- cyx/media/libre_office.py
+++ cyx/media/libre_office.py
@@ -6,22 +6,28 @@ import subprocess
 import uuid
 import os
 
+
 class LibreOfficeService:
     def __init__(self):
         self.config = config
         self.libre_office_path = self.config.libre_office_path
         self.working_dir = pathlib.Path(__file__).parent.parent.parent.__str__()
-        self.temp_dir = os.path.join(self.working_dir,"tmp","libre-office")
+        self.temp_dir = os.path.join(self.working_dir, "tmp", "libre-office")
         if not os.path.isdir(self.temp_dir):
-            os.makedirs(self.temp_dir,exist_ok=True)
-        self.user_profile_dir = os.path.join(self.temp_dir,"users-profiles")
+            os.makedirs(self.temp_dir, exist_ok=True)
+        self.user_profile_dir = os.path.join(self.temp_dir, "users-profiles")
         if not os.path.isdir(self.user_profile_dir):
-            os.makedirs(self.user_profile_dir,exist_ok=True)
-        self.output_dir = os.path.join(self.temp_dir,"out-put")
+            os.makedirs(self.user_profile_dir, exist_ok=True)
+        self.output_dir = os.path.join(self.temp_dir, "out-put")
         if not os.path.isdir(self.output_dir):
-            os.makedirs(self.output_dir,exist_ok=True)
+            os.makedirs(self.output_dir, exist_ok=True)
 
-    def get_image(self, file_path)->str:
+    def get_image(self, file_path) -> str:
+        """
+        Generate image of file by using libre office
+        :param file_path:
+        :return:
+        """
         filename_only = pathlib.Path(file_path).stem
         ret_file = os.path.join(self.output_dir, f"{filename_only}.png")
         if os.path.isfile(ret_file):
@@ -50,5 +56,43 @@ class LibreOfficeService:
         )
         ret = pid.wait()  # Đợi
 
+        return ret_file
+
+    def extract_page(self, file_path, page_number):
+        """
+                Generate image of file by using libre office
+                :param file_path:
+                :return:
+                """
+        filename_only = pathlib.Path(file_path).stem
+        file_extension = os.path.splitext(file_path)[1][1:]
+        ret_file = os.path.join(self.output_dir, f"{filename_only}.{file_extension}")
+        if os.path.isfile(ret_file):
+            return ret_file
+
+        uno = f"Negotiate=0,ForceSynchronous=1;"
+        # from subprocess import CREATE_NEW_CONSOLE
+
+        user_profile_id = str(uuid.uuid4())  # Tạo user profile giả, nếu kg có điều này LibreOffice chỉ tạo 1 instance,
+        # kg xử lý song song được
+        full_user_profile_path = os.path.join(self.user_profile_dir, user_profile_id)
+        #convert longfile.pdf[0-1,3] output.pdf //libre office convert
+        #pdf:draw_pdf_Export:{"PageRange":{"type":"string","value":"2-"}}
+        pid = subprocess.Popen(
+            [
+                self.libre_office_path,
+                '--headless',
+                '--convert-to', 'pdf:draw_pdf_Export:{"PageRange":{"type":"string","value":"2-"}}',
+                f"--accept={uno}",
+                f"-env:UserInstallation=file://{full_user_profile_path.replace(os.sep, '/')}",
+                '--outdir',
+                self.output_dir, f"{file_path}"
+            ],
+            shell=False,
+            start_new_session=True
+            # creationflags=16
+        )
+
+        ret = pid.wait()  # Đợi
 
         return ret_file
