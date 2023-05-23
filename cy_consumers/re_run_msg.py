eccb2ad51fc5eb32c2481e425bdf9b39a32a669e
nttlong <zugeliang2000@gmail.com>
2023-05-23 17:14:10 +0700

    first commit

--- cy_consumers/re_run_msg.py
+++ cy_consumers/re_run_msg.py
@@ -1,7 +1,31 @@
+import cy_docs
 from cyx.common.msg import MessageService, MessageInfo
 from cyx.common.rabitmq_message import RabitmqMsg
 from cy_xdoc.services.files import FileServices
+from cy_xdoc.services.apps import AppServices
+import cyx.common.msg
 import cy_kit
-broker:MessageService = cy_kit.singleton(MessageService)
+# cy_kit.config_provider(
+#         from_class=MessageService,
+#         implement_class=RabitmqMsg
+#     )
+
+msg = cy_kit.singleton(RabitmqMsg)
 files = cy_kit.singleton(FileServices)
-items = files.find_file_async()
+apps = cy_kit.singleton(AppServices)
+apps_list = apps.get_list("admin")
+for app in apps_list:
+    qr = files.get_queryable_doc(app.Name)
+    items = qr.context.aggregate().match(
+        ((qr.fields.HasThumb == False) | (cy_docs.not_exists(qr.fields.HasThumb))) & \
+        (cy_docs.EXPR(qr.fields.SizeInBytes ==qr.fields.SizeUploaded))
+    ).sort(
+        qr.fields.RegisterOn.desc()
+    ).limit(100)
+    for x in items:
+        msg.emit(
+            app_name=app.Name,
+            message_type=cyx.common.msg.MSG_FILE_UPLOAD,
+            data=x
+        )
+
