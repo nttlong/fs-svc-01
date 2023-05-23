c1ef1149665d037077939c4bd48f61a65be930d6
nttlong <zugeliang2000@gmail.com>
2023-05-23 15:24:04 +0700

    first commit

--- docker-build/data-set-check.sh
+++ docker-build/data-set-check.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-mkdir -p /app/dataset/chek-version
+mkdir -p /app/share-storage/dataset
+mkdir -p /app/share-storage/dataset/chek-version
 
+mv /app-dataset/deepdoctection /app/share-storage/dataset
+mv /app-dataset/doctr /app/share-storage/dataset
+mv /app-dataset/easyocr /app/share-storage/dataset
+mv /app-dataset/huggingface /app/share-storage/dataset
+mv /app-dataset/matplotlib /app/share-storage/dataset
+mv /app-dataset/torch /app/share-storage/dataset
+mv /app-dataset/weights /app/share-storage/dataset
+mv /app-dataset/conf_dd_one.yaml /app/share-storage/dataset
