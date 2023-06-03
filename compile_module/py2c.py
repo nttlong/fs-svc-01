80a449c81514ac130862d3b136757945cea6be3c
Nttlong <zugeliang2000@gmail.com>
2023-06-03 12:57:58 +0700

    Update py2c.py

--- compile_module/py2c.py
+++ compile_module/py2c.py
@@ -2,51 +2,126 @@ import pathlib
 import subprocess
 import sys
 import os
-working_dir = pathlib.Path(__file__).parent.parent.__str__()
-def get_content(file_path:str):
-    file_name = pathlib.Path(file_path).name
+working_dir = pathlib.Path(__file__).parent.__str__()
+def get_content_with_files(files, module_name):
+    file_paths = "".join([f"\t\tr'{x}',\n" for x in files])
+    file_paths=file_paths.rstrip('\n,').lstrip('\t')
+    name = f"{module_name}"
     r = f"" \
-        f"import pathlib\n" \
         f"from setuptools import setup\n" \
         f"from Cython.Build import cythonize\n" \
-        f"import os\n" \
-        f"build_dir = pathlib.Path(__file__).parent.__str__()\n" \
-        f"file_path=os.path.join(build_dir, '{file_name}')\n" \
         f"setup(\n" \
-        f"name='files_services',\n" \
-        f"ext_modules=cythonize(file_path),\n" \
-        f"zip_safe=True,\n" \
-        f"packages=['{file_name}'])\n"
+        f"\text_modules=cythonize([{file_paths}],language_level = '3')\n" \
+        f")\n"
     return r
-if len(sys.argv)<2:
-    print("miss directory to module")
-compile_dir = sys.argv[1]
-full_compiler_dir = os.path.join(working_dir,compile_dir)
-root_dir,dirs,files = list(os.walk(full_compiler_dir))[0]
-files_list = []
-for file in files:
-    if file=="__init__.py":
-        continue
-    if os.path.splitext(file)[1]!=".py": continue
-    if file.startswith("compiler."): continue
-    compile_file = f"compiler.{file}"
-    full_path_to_compile_file = os.path.join(root_dir,compile_file)
-    content = get_content(os.path.join(root_dir,file))
-    if not os.path.exists(full_path_to_compile_file):
-        with open(full_path_to_compile_file,"w+") as f:
-            f.write(content)
-    files_list +=[compile_file]
+def get_content(file_path:str,package_name:str):
+    file_name = pathlib.Path(file_path).name
+    name  =f"{package_name}"
+    r = f"" \
+        f"from setuptools import setup\n" \
+        f"from Cython.Build import cythonize\n" \
+        f"setup(\n" \
+        f"ext_modules=cythonize([r'{file_path}'],language_level = '3'),\n" \
+        f")\n"
+    return r
+
+
+def generate_setup_file(full_gen_file, rel_dir):
+    content = get_content(full_gen_file,rel_dir.replace(os.path.sep,'.').lstrip('.').rstrip('.'))
+    if os.path.exists(full_gen_file):
+        os.remove(full_gen_file)
+    with open(full_gen_file,"w+") as f:
+        f.write(content)
+
+def get_list_of_file(full_compiler_dir):
+    ret = []
+    global working_dir
+    root_dir, dirs, files = list(os.walk(full_compiler_dir))[0]
+
+
+    for file in files:
+        if os.path.splitext(file)[1] != ".py":
+            continue
+        if pathlib.Path(file).name=="__init__.py":
+            continue
+        ret += [os.path.join(root_dir,file)]
+    for dif in dirs:
+        full_dir = os.path.join(root_dir, dif)
+        fx = get_list_of_file(full_dir)
+        ret += fx
+    return ret
+
+def generate_setup_file_files(ouput_dir, module_name, content):
+    global working_dir
+    full_out_dir = os.path.join(working_dir,ouput_dir)
+    os.makedirs(full_out_dir,exist_ok=True)
+
+    full_gen_file = os.path.join(full_out_dir,f"{module_name}_setup.py")
+    if os.path.exists(full_gen_file):
+        os.remove(full_gen_file)
+    with open(full_gen_file, "w+") as f:
+        f.write(content)
+    return full_gen_file
+
+def generate_compile_file(dir:str=None):
+    ret = []
+    global working_dir
+    root_dir, dirs, files = list(os.walk(dir))[0]
+
+    tem_dir_compile = os.path.join(working_dir,"cython-compile-temp")
+    os.makedirs(tem_dir_compile,exist_ok=True)
+    rel_dir = dir[len(working_dir):]
+
+    for file in files:
+        if os.path.splitext(file)[1]!=".py":
+            continue
+        if rel_dir == "":
+            gen_file = file
+        else:
+            gen_file = ".".join([rel_dir.replace(os.path.sep,"."),file]).lstrip('.')
+        full_gen_file = os.path.join(tem_dir_compile,gen_file)
+        print(f"Generate file {full_gen_file} ...")
+
+        print(f"Generate file {full_gen_file} is complete")
+        ret+=[full_gen_file]
+    for dif in dirs:
+        full_dir = os.path.join(root_dir,dif)
+        fx = generate_compile_file(full_dir)
+        ret+=fx
+    return ret
+
+
+full_compiler_dir = sys.argv[1]
+    #python cy_es/setup.py build_ext --inplace
+    #test/models
+# list_of_files = generate_compile_file(full_compiler_dir)
+
+
+list_of_files  = get_list_of_file(full_compiler_dir)
 os.chdir(working_dir)
 
-for f in files_list:
-    print(f"Compile file {f}")
 
-    cmd_compile = f"{sys.executable} {f}  build_ext --inplace"
-    # subprocess.Popen(cmd_compile, cwd=root_dir)
 
-    ret = subprocess.check_output([
-         sys.executable,os.path.join(compile_dir,f),
-        'build_ext','--inplace'])
-    str_ret= ret.decode('utf8')
-    print(str_ret)
-    #python cy_es/setup.py build_ext --inplace
+
+
+content = get_content_with_files(files =list_of_files,module_name="test")
+
+
+
+
+
+full_file = generate_setup_file_files(ouput_dir ="c-setup-tem", module_name = "test",content = content)
+print(full_file)
+
+cmd = [
+     sys.executable,full_file,
+    'build_ext','--inplace']
+
+command_line  = " ".join(cmd)
+print(" ".join(cmd))
+# subprocess.Popen(cmd_compile, cwd=root_dir)
+subprocess.call(command_line)
+# ret = subprocess.check_output(cmd)
+# str_ret= ret.decode('utf8')
+# print(str_ret)
+#python cy_es/setup.py build_ext --inplace
