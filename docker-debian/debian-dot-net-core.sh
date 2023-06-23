5268f575e5440a1db29885d4352e39b3bb4bb272
nttlong <zugeliang2000@gmail.com>
2023-06-23 14:09:12 +0700

    first commit

--- docker-debian/debian-dot-net-core.sh
+++ docker-debian/debian-dot-net-core.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 export user=xdoc
-export platform_=linux/amd64
 export platform=linux/amd64,linux/arm64/v8
 export generation=1
 export repositiory=docker.lacviet.vn
 export push=docker.lacviet.vn/xdoc
-docker_file=debian-libre-office-headless
+docker_file=debian-dot-net-core
 docker --log-level "info" buildx build \
       --build-arg REPO_LOCATION=$repositiory \
       --build-arg TAG="$1" \
       --build-arg USER=$user -t \
       $push/$docker_file:$generation  \
-      --platform=$platform ./.. -f $docker_file  --push=true --output type=registry
+      --platform=linux/amd64,linux/arm64/v8 ./.. -f $docker_file  --push=true --output type=registry
